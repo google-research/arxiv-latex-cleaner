google-research/arxiv-latex-cleaner

commit 8310b0f8be3682165c9022d4053d7bab26c9798f

adds `--config` argument

arxiv_latex_cleaner/__main__.py
@@ -22,10 +22,11 @@ Main module for ``arxiv_latex_cleaner``
 """
 
 from ._version import __version__
-from .arxiv_latex_cleaner import run_arxiv_cleaner
+from .arxiv_latex_cleaner import run_arxiv_cleaner, merge_args_into_config
 
 import argparse
 import json
+import yaml
 
 PARSER = argparse.ArgumentParser(
     prog="arxiv_latex_cleaner@{0}".format(__version__),
@@ -84,9 +85,27 @@ PARSER.add_argument(
 )
 
 PARSER.add_argument(
-    "--use_external_tikz", type=str, help="Folder (relative to input folder) containing externalized tikz figures in PDF format."
+    "--config",
+    type=str,
+    help="Read settings from `.yaml` config file. If command line arguments are provided additionally, the config file parameters are updated with the command line parameters.",
+    required=False,
 )
 
 ARGS = vars(PARSER.parse_args())
-run_arxiv_cleaner(ARGS)
+
+
+if ARGS["config"] is not None:
+    try:
+        with open(ARGS["config"], "r") as config_file:
+            config_params = yaml.safe_load(config_file)
+        final_args = merge_args_into_config(ARGS, config_params)
+
+    except FileNotFoundError:
+        print(f"config file {ARGS.config} not found.")
+        final_args = ARGS
+        final_args.pop("config", None)
+else:
+    final_args = ARGS
+
+run_arxiv_cleaner(final_args)
 exit(0)

arxiv_latex_cleaner/arxiv_latex_cleaner.py
@@ -409,3 +409,22 @@ def run_arxiv_cleaner(parameters):
     _copy_file(non_tex_file, parameters)
 
   _resize_and_copy_figures_if_referenced(parameters, full_content, splits)
+
+
+def merge_args_into_config(args, config_params):
+    final_args = copy.deepcopy(config_params)
+    config_keys = config_params.keys()
+    for key, value in args.items():
+        if key in config_keys:
+            if any([isinstance(value, t) for t in [str, bool, float, int]]):
+                # overwrite config value with args value
+                final_args[key] = value
+            elif isinstance(value, list):
+                # append args values to config values
+                final_args[key] = value + config_params[key]
+            elif isinstance(value, dict):
+                # update config params with args params
+                final_args[key].update(**value)
+        else:
+            final_args[key] = value
+    return final_args

arxiv_latex_cleaner/tests/arxiv_latex_cleaner_test.py
@@ -20,10 +20,70 @@ import unittest
 from absl.testing import parameterized
 
 from arxiv_latex_cleaner import arxiv_latex_cleaner
+from arxiv_latex_cleaner.arxiv_latex_cleaner import merge_args_into_config
 from PIL import Image
 
 
+def make_args(
+    input_folder="foo/bar",
+    resize_images=False,
+    im_size=500,
+    compress_pdf=False,
+    pdf_im_resolution=500,
+    images_whitelist={},
+    commands_to_delete=[],
+    use_external_tikz="foo/bar/tikz",
+):
+    args = {
+        "input_folder": input_folder,
+        "resize_images": resize_images,
+        "im_size": im_size,
+        "compress_pdf": compress_pdf,
+        "pdf_im_resolution": pdf_im_resolution,
+        "images_whitelist": images_whitelist,
+        "commands_to_delete": commands_to_delete,
+        "use_external_tikz": use_external_tikz,
+    }
+    return args
+
+
 class UnitTests(parameterized.TestCase):
+    @parameterized.named_parameters(
+        {
+            "testcase_name": "empty config",
+            "args": make_args(),
+            "config_params": {},
+            "final_args": make_args(),
+        },
+        {
+            "testcase_name": "empty args",
+            "args": {},
+            "config_params": make_args(),
+            "final_args": make_args(),
+        },
+        {
+            "testcase_name": "args and config provided",
+            "args": make_args(
+                images_whitelist={"path1/": 1000}, commands_to_delete=[r"\todo1"]
+            ),
+            "config_params": make_args(
+                "foo_/bar_",
+                True,
+                1000,
+                True,
+                1000,
+                images_whitelist={"path2/": 1000},
+                commands_to_delete=[r"\todo2"],
+                use_external_tikz="foo_/bar_/tikz_",
+            ),
+            "final_args": make_args(
+                images_whitelist={"path1/": 1000, "path2/": 1000},
+                commands_to_delete=[r"\todo1", r"\todo2"],
+            ),
+        },
+    )
+    def test_merge_args_into_config(self, args, config_params, final_args):
+        self.assertEqual(merge_args_into_config(args, config_params), final_args)
 
   @parameterized.named_parameters(
       {

commit 2915034b8c4bf7f2a74b32dc8f71ddf68c047a1c

Fix external_tikz regression
Fix a regression introduced by commit 8310b0f8be3682165c9022d4053d7bab26c9798f which removed the --use_external_tikz command-line switch

arxiv_latex_cleaner/__main__.py
@@ -84,6 +84,13 @@ PARSER.add_argument(
 )
 
 PARSER.add_argument(
+    "--use_external_tikz",
+    type=str,
+    help="Folder (relative to input folder) containing externalized tikz "
+         "figures in PDF format."
+)
+
+PARSER.add_argument(
     "--config",
     type=str,
     help=("Read settings from `.yaml` config file. If command line arguments "